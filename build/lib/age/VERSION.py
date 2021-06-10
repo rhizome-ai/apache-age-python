31d1615b4b7b3ec395172f9b4b8e550616ff7b47
kighie <kighie@hanwha.com>
2021-06-10 22:11:12 +0900

    clean3

--- build/lib/age/VERSION.py
+++
@@ -1,7 +0,0 @@
-
-
-VER_MAJOR = 0
-VER_MINOR = 0
-VER_MICRO = 1
-
-VERSION = '.'.join([str(VER_MAJOR),str(VER_MINOR),str(VER_MICRO)])
--- build/lib/age/graph.py
+++
@@ -1,5 +0,0 @@
-
-
-class Graph:
-    def __init__(self, stmt=None) -> None:
-        self.statement = stmt
